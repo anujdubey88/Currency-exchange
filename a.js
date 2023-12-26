29a972c86e5fdfc496b1adec9c39d3a7c0098d63
anujdubey88 <127838317+anujdubey88@users.noreply.github.com>
2023-12-26 11:05:18 +0530

    currancy_exchange

--- a.js
+++ a.js
@@ -1,5 +1,6 @@
 const BASE_URL ="https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
 
+
 const dropdowns=document.querySelectorAll(".dropdown select");
 const btn=document.querySelector(" form button");
 const fromCurr=document.querySelector(".from select");
